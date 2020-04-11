6748f1d4224b83e8cd9fe6e521a42546c93adc38
Ubuntu <wgwj1234@gmail.com>
2020-04-11 23:38:33 +0000

    another commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("vendor", "javascripts") 
 Rails.application.config.assets.paths << Rails.root.join("vendor", "stylesheets")
 
+
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
