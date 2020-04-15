0177dd03c4152a09da70501c968dee7d17e1c584
root <wgwj1234@gmail.com>
2020-04-15 04:28:39 +0000

    asdad

---
+++ app/helpers/spree/base_helper_decorator.rb
@@ -0,0 +1,9 @@
+module Spree
+  module BaseHelperDecorator
+      
+        def meta_image_data_tag
+          tag('meta', property: 'og:image', content: "https://images.unsplash.com/photo-1457530378978-8bac673b8062?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" ) if meta_image_url_path
+        end
+        
+   end 
+end
