12d6b917cd96b18facac421e14f3cd1c7f91f0eb
root <wgwj1234@gmail.com>
2020-04-21 11:30:53 +0000

    sadf

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-get '/naver072a084090e5defb8e32b6ac8e241188' => "spree/home#naver072a084090e5defb8e32b6ac8e241188"
+  #get '/naver072a084090e5defb8e32b6ac8e241188' => "spree/home#naver072a084090e5defb8e32b6ac8e241188"
 get '/import' => "spree/checkout#import"
   
 get '/introduce' => "spree/home#introduce"
