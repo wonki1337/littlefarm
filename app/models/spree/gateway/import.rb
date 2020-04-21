16fb7ab93952b89a1a5a99161078ff266ebccb05
root <wgwj1234@gmail.com>
2020-04-21 12:34:33 +0000

    werwe

--- app/models/spree/gateway/import.rb
+++ app/models/spree/gateway/import.rb
@@ -1,20 +1,16 @@
 class Spree::Gateway::Import < Spree::Gateway
-    
-  # def provider_class
-  #   Spree::Gateway::Import
-  # end
-  # def payment_source_class
-  #   Spree::CreditCard
-  # end
+  def provider_class
+    Spree::Gateway::Import
+  end
+  def payment_source_class
+    Spree::CreditCard
+  end
 
-  # def method_type
-  #   'import'
-  # end
+  def method_type
+    'Import'
+  end
 
-  # def purchase(amount, transaction_details, options = {})
-  #   ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
-  # end
-  
-    
-  
+  def purchase(amount, transaction_details, options = {})
+    ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
+  end
 end
